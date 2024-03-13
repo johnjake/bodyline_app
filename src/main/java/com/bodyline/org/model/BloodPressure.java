d8f93ccecd85e99f78a27326d2e8a9a295bcc95d
RonvieTiongzon <ronvietiongzon00@gmail.com>
2024-03-13 20:28:51 +0800

    BloodPressure model added

---
+++ src/main/java/com/bodyline/org/model/BloodPressure.java
@@ -0,0 +1,37 @@
+package com.bodyline.org.model;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("Advantage_Card")
+public class BloodPressure {
+
+    @Id
+    @Column("BP_ControlID")
+    private Integer bpControlId;
+
+
+    @Column("Ctrl_ID")
+    private Integer ctrlId;
+
+    @Column("Barcode")
+    private String barcode;
+
+    @Column("BP")
+    private String bp;
+
+    @Column("Weight")
+    private Float weight;
+
+    @Column("Date_Taken")
+    private Date dateTaken;
+}
