e5f366f1dc25a2b215b0844964bd4181fa07c036
RonvieTiongzon <ronvietiongzon00@gmail.com>
2024-03-13 20:57:19 +0800

    CA model added

--- src/main/java/com/bodyline/org/model/CA.java
+++ src/main/java/com/bodyline/org/model/CA.java
@@ -1,14 +1,38 @@
 package com.bodyline.org.model;
 
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("Customers")
+@Table("Blood_Pressure")
 public class CA {
+
+    @Id
+    @Column("CA_ID")
+    private Integer caId;
+
+    @Column("Ctrl_ID")
+    private Integer ctrlId;
+
+    @Column("Amount")
+    private BigDecimal amount;
+
+    @Column("Purpose")
+    private String purpose;
+
+    @Column("Receipt_Num")
+    private String receiptNumber;
+
+    @Column("Ca_Date")
+    private Date caDate;
 
 }
