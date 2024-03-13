68067ecc7e8d226a537ca86f70f61003b1bd381d
RonvieTiongzon <ronvietiongzon00@gmail.com>
2024-03-13 22:08:05 +0800

    Daily Expenses model added

---
+++ src/main/java/com/bodyline/org/model/Daily_Expenses.java
@@ -0,0 +1,30 @@
+package com.bodyline.org.model;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("Daily_Expenses")
+public class Daily_Expenses {
+
+    @Id
+    @Column("ID")
+    private Integer id;
+
+    @Column("TOTAL_EXPENSES")
+    private BigDecimal totalExpenses;
+
+    @Column("DATE_EXPENSES")
+    private Date dateExpenses;
+
+
+}
