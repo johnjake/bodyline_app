111e5ab836b84aa18bdce8a2f3a86f85bd07bf31
RonvieTiongzon <ronvietiongzon00@gmail.com>
2024-03-13 20:40:59 +0800

    Initial CA model

---
+++ src/main/java/com/bodyline/org/model/CA.java
@@ -0,0 +1,14 @@
+package com.bodyline.org.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("Customers")
+public class CA {
+
+}
