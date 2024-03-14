8d1da8ed47950d5a277b5f23f79ffb3b03e48bbb
RonvieTiongzon <127647177+RonvieTiongzon@users.noreply.github.com>
2024-03-14 13:18:06 +0800

    Gross Sales Model added (#14)
    * Gross Sales Model added
    
    * Fixed spacing issue

---
+++ src/main/java/com/bodyline/org/model/GrossSales.java
@@ -0,0 +1,51 @@
+package com.bodyline.org.model;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("Gross_Sales")
+public class GrossSales {
+
+    @Id
+    @Column("Sales_Control")
+    private Integer salesControl;
+
+    @Column("SalesID")
+    private String salesId;
+
+    @Column("Invoice_Ctrl")
+    private Integer invoiceCtrl;
+
+    @Column("InvoiceID")
+    private String invoiceId;
+
+    @Column("Gross")
+    private BigDecimal gross;
+
+    @Column("Discount")
+    private BigDecimal discount;
+
+    @Column("Discount_Type")
+    private String discountType;
+
+    @Column("Net")
+    private BigDecimal net;
+
+    @Column("User_Control")
+    private Integer userControl;
+
+    @Column("User_Name")
+    private String userName;
+
+    @Column("Sales_Date")
+    private Date salesDate;
+}
